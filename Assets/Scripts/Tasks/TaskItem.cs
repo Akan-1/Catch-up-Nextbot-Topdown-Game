0563311b605f32a05f4f11f8eb0132dd5267cb90
ValterGames <68495491+ValterGames-Coder@users.noreply.github.com>
2023-04-15 08:56:22 +0300

    little changes

--- Assets/Scripts/Tasks/TaskItem.cs
+++ Assets/Scripts/Tasks/TaskItem.cs
@@ -3,14 +3,13 @@ using UnityEngine.Events;
 
 public class TaskItem : MonoBehaviour
 {
-    public UnityEvent OnUse;
+    public UnityEvent onUse;
+    [SerializeField] private bool _delete;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.TryGetComponent(out PlayerController player))
-        {
-            OnUse?.Invoke();
-            gameObject.SetActive(false);
-        }
+        if (!other.TryGetComponent(out PlayerController _)) return;
+        onUse?.Invoke();
+        gameObject.SetActive(!_delete);
     }
 }
