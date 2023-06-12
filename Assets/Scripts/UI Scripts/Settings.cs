bd52bb160cd4cf0931f1fea0f4f7c742651dfbba
ValterGames <68495491+ValterGames-Coder@users.noreply.github.com>
2023-06-13 00:08:11 +0300

    Update Settings.cs

--- Assets/Scripts/UI Scripts/Settings.cs
+++ Assets/Scripts/UI Scripts/Settings.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
 using TMPro;
@@ -9,9 +10,21 @@ public class Settings : MonoBehaviour
     [SerializeField] private string _parameterName;
     [SerializeField] private string _sliderName;
 
+    private void Start()
+    {
+        if (_audioMixer != null)
+        {
+            float volume = PlayerPrefs.GetFloat("AudioVolume");
+            _audioMixer.SetFloat(_parameterName, Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("AudioVolume", volume);
+            _text.text = _sliderName + Mathf.Round(volume * 100) + "%";
+        }
+    }
+
     public void SetVolume(float volume)
     {
         _audioMixer.SetFloat(_parameterName, Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("AudioVolume", volume);
         _text.text = _sliderName + Mathf.Round(volume * 100) + "%";
     }
 }
